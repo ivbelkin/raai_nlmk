9daaf95bc613ee4e4cb164b773b68e1f508a1b93
Ilya Belkin <ilya.belkin-trade@yandex.ru>
2019-06-30 22:08:20 +0300

    add продолжительность_завалки feature

--- nlmk/dataset.py
+++ nlmk/dataset.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+from tqdm import tqdm
 
 
 class Dataset:
@@ -24,27 +25,31 @@ class Dataset:
         self.categorical = ["номер_клетки", "номер_валка", "положение_в_клети", "материал_валка"]
         self.alphas = [0, 0, 0, 0]
 
+        self.add_zavalka_number()
+        self.handle_datetime()
+
     def get_train_valid_data(self):
-        train_df = self.zavalki_df.query("дата_завалки < '{}'".format(self.train_valid_split))
-        valid_df = self.zavalki_df.query("дата_завалки >= '{}'".format(self.train_valid_split))
+        train_df = self.zavalki_df.query("дата_завалки < '{}'".format(self.train_valid_split)).drop("дата_завалки", axis=1)
+        valid_df = self.zavalki_df.query("дата_завалки >= '{}'".format(self.train_valid_split)).drop("дата_завалки", axis=1)
         return self.prepare_X_y(train_df, valid_df)
 
     def get_train_test_data(self):
-        train_df = self.zavalki_df.query("дата_завалки >= '{}'".format(self.train_test_split))
-        return self.prepare_X_y(train_df, self.test_df)
+        train_df = self.zavalki_df.query("дата_завалки >= '{}'".format(self.train_test_split)).drop("дата_завалки", axis=1)
+        test_df = self.test_df.drop("дата_завалки", axis=1)
+        return self.prepare_X_y(train_df, test_df)
 
     def prepare_X_y(self, train_df, valid_df):
         train_df["положение_в_клети"] = train_df["положение_в_клети"].map(self.cat_to_int["положение_в_клети"])
         valid_df["положение_в_клети"] = valid_df["положение_в_клети"].map(self.cat_to_int["положение_в_клети"])
 
-        train_X = train_df.drop(["дата_завалки", "дата_вывалки", "износ"], axis=1)
+        train_X = train_df.drop(["износ"], axis=1)
         train_y = train_df["износ"]
 
         if "износ" in valid_df.columns:
-            valid_X = valid_df.drop(["дата_завалки", "дата_вывалки", "износ"], axis=1)
+            valid_X = valid_df.drop(["износ"], axis=1)
             valid_y = valid_df["износ"]
         else:
-            valid_X = valid_df.drop(["дата_завалки", "дата_вывалки"], axis=1)
+            valid_X = valid_df.copy()
             valid_y = None
 
         if "id" in valid_df.columns:
@@ -54,6 +59,9 @@ class Dataset:
         valid_X = valid_X.merge(self.valki_df, on="номер_валка", how="left")
 
         train_X, valid_X = self.mean_target(train_X, train_y, valid_X)
+
+        train_X = train_X.drop("номер_завалки", axis=1)
+        valid_X = valid_X.drop("номер_завалки", axis=1)
 
         if valid_y is not None:
             return train_X, train_y, valid_X, valid_y
@@ -80,3 +88,33 @@ class Dataset:
             n = valid_X[fname].map(counts)
             valid_X[fname] = (valid_X[fname].map(mapping) * n + alpha * global_mean * N) / (n + alpha * N)
         return train_X, valid_X
+
+    def add_zavalka_number(self):
+        train_zavalki_dates = sorted(set(self.zavalki_df["дата_завалки"]))
+        test_zavalki_dates = sorted(set(self.test_df["дата_завалки"]))
+        zavalki_dates = train_zavalki_dates + test_zavalki_dates
+        mapping = {z: i for i, z in enumerate(zavalki_dates)}
+
+        self.zavalki_df["номер_завалки"] = self.zavalki_df["дата_завалки"].map(mapping)
+        self.test_df["номер_завалки"] = self.test_df["дата_завалки"].map(mapping)
+
+        zn = 0
+        self.ruloni_df["номер_завалки"] = -1
+        N = len(zavalki_dates)
+        zavalki_number = []
+        for i in tqdm(range(len(self.ruloni_df))):
+            if zn < N - 1 and self.ruloni_df["Время_обработки"].iloc[i] >= zavalki_dates[zn + 1]:
+                zn += 1
+            zavalki_number.append(zn)
+        self.ruloni_df["номер_завалки"] = zavalki_number
+
+    def handle_datetime(self):
+        t1 = self.zavalki_df["дата_завалки"].map(pd.to_datetime)
+        t2 = self.zavalki_df["дата_вывалки"].map(pd.to_datetime)
+        self.zavalki_df["продолжительность_завалки"] = list(map(lambda x: x.seconds, t2 - t1))
+        self.zavalki_df = self.zavalki_df.drop(["дата_вывалки"], axis=1)
+
+        t1 = self.test_df["дата_завалки"].map(pd.to_datetime)
+        t2 = self.test_df["дата_вывалки"].map(pd.to_datetime)
+        self.test_df["продолжительность_завалки"] = list(map(lambda x: x.seconds, t2 - t1))
+        self.test_df = self.test_df.drop(["дата_вывалки"], axis=1)
